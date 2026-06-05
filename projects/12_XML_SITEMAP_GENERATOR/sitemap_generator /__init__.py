cffa97327b0b3a227ee4e879d200b6b4947fe90f
Kiran Kumar V <imkirankumar23@gmail.com>
2026-06-05 13:22:56 +0530

    Delete projects/12_XML_SITEMAP_GENERATOR/sitemap_generator  directory

--- projects/12_XML_SITEMAP_GENERATOR/sitemap_generator /__init__.py
+++
@@ -1,7 +0,0 @@
-from .core import generate_sitemap, generate_sitemap_index, generate_sitemap_from_file
-
-__all__ = [
-    "generate_sitemap",
-    "generate_sitemap_index",
-    "generate_sitemap_from_file",
-]
