fdacc384b1220bb367260e813ecd82778fcd8d4b
Kiran Kumar V <imkirankumar23@gmail.com>
2026-06-05 13:22:20 +0530

    Add files via upload

---
+++ projects/12_XML_SITEMAP_GENERATOR/sitemap_generator/__init__.py
@@ -0,0 +1,7 @@
+from .core import generate_sitemap, generate_sitemap_index, generate_sitemap_from_file
+
+__all__ = [
+    "generate_sitemap",
+    "generate_sitemap_index",
+    "generate_sitemap_from_file",
+]
