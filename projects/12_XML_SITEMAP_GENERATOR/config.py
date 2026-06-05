cf2c8c5f3b5d90c39515b92bd813c84f952ee6c6
Kiran Kumar V <imkirankumar23@gmail.com>
2026-06-05 13:11:21 +0530

    Add files via upload

---
+++ projects/12_XML_SITEMAP_GENERATOR/config.py
@@ -0,0 +1,5 @@
+# Configuration for the sitemap generator
+MAX_URLS_PER_SITEMAP = 50000   # Google's limit
+SITEMAP_NAMESPACE = "http://www.sitemaps.org/schemas/sitemap/0.9"
+OUTPUT_DIR = "output"
+DEFAULT_SITEMAP_FILENAME = "sitemap.xml"
